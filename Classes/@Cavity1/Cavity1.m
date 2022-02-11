93fbafeeab1795b57471d8c1007093f2f2074ea9
Nils Melchert <nils.melchert@imr.uni-hannover.de>
2022-02-11 17:17:07 +0100

    start rethinking oop

--- Classes/@Cavity1/Cavity1.m
+++ Classes/@Cavity1/Cavity1.m
@@ -1,4 +1,4 @@
-classdef Cavity1
+classdef Cavity1 < handle
     %Cavity1 the class used to define cavity with thin mirrors
     % To define a cavity use
     % C1 = Cavity1(I1,I2,L,E1) with C1 the cavity defined, I1 and I2 2
@@ -28,16 +28,26 @@ classdef Cavity1
         Field_ref = [];
         Field_trans = [];
         Field_reso_guess = [];
-        Loss_RTL = [];
+        Loss_RTL = [];                
         
     end
     
     methods
-        function C = Cavity1(varargin)
+        % Declare methods which are implemented in external files
+        [] = declare_on_gpu(obj)
+        [power_buildup] = calculate_fields(obj, varargin)
+        [] = calculate_fields_ac(obj, varargin)
+        [length_scan, power_scan] = scan(obj, varargin)        
+    end
+    
+    methods
+        
+        
+        function obj = Cavity1(varargin)
             switch nargin
                 case{0,1,2,3}
                     error('Cavity1(): at least 4 arguments must be given: 2 interfaces, a length and the input laser beam')
-                case 4
+                case {4,5}
                     if  ~(isa(varargin{1}, 'Interface') || isa(varargin{1}, 'Mirror'))
                         error('Cavity1(): the first argument must be an instance of the class Interface or Mirror')
                     end
@@ -52,36 +62,43 @@ classdef Cavity1
                     
                     if  ~isa(varargin{4}, 'E_Field')
                         error('Cavity1(): the fourth argument, the input laser beam must be an instance of the class E_field')
-                    end
+                    end                                        
                     
+                    obj.I_input = varargin{1};
+                    obj.I_end = varargin{2};
+                    obj.Length = varargin{3};
+                    obj.Laser_in = varargin{4};
                     
-                    C.I_input = varargin{1};
-                    C.I_end = varargin{2};
-                    C.Length = varargin{3};
-                    C.Laser_in = varargin{4};
-                    
-                    C.Propagation_mat = Prop_operator(C.Laser_in,C.Length);
+                    obj.Propagation_mat = Prop_operator(obj.Laser_in, obj.Length);
                  
                 % Check if the input laser is optimally mode matched    
-                if  C.Laser_in.Optimal_mode_matching
-                    Beam_paramater = Check_Stability(C,'Display',false);
-                    New_input_field =  E_Field(Grid(C),'w',Beam_paramater(1),'R',Beam_paramater(2),'mode',C.Laser_in.Mode_name);
+                if  obj.Laser_in.Optimal_mode_matching
+                    Beam_paramater = Check_Stability(obj,'Display',false);
+                    New_input_field =  E_Field(Grid(obj),'w',Beam_paramater(1),'R',Beam_paramater(2),'mode',obj.Laser_in.Mode_name);
                     % add the sidebands as it used to be
-                    for ii = 1:C.Laser_in.Nb_Pair_SB
-                        New_input_field = Add_Sidebands(New_input_field,'Mod_freq',C.Laser_in.SB(ii).Frequency_Offset,'Mod_index',C.Laser_in.SB(ii).Input_Mod_index);
+                    for ii = 1:obj.Laser_in.Nb_Pair_SB
+                        New_input_field = Add_Sidebands(New_input_field,'Mod_freq',obj.Laser_in.SB(ii).Frequency_Offset,'Mod_index',obj.Laser_in.SB(ii).Input_Mod_index);
                     end
-                    C.Laser_in = New_input_field;
+                    obj.Laser_in = New_input_field;
                 end    
+                
+                if (nargin == 5)
+                    if  ~islogical(varargin{5})
+                        error('Cavity1(): the fifth optional argument must be bool and specifies, weather or not to use the GPU (defaults to false)')
+                    end   
+                    obj.declare_on_gpu()                    
+                end
                     
                 otherwise
                     disp('Cavity1(): invalid number of input arguments, cavity not created')
-                    
+                   
             end
         end
-        
+                    
         function G = Grid(obj)
             G = obj.Laser_in.Grid;
-        end
+        end                
+
         
     end
     
