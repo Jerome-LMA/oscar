7fb1a32f033fa884c80a70da6a0025cdaa8b8cbd
Jerome Degallaix <j.degallaix@lma.in2p3.fr>
2022-02-22 12:25:13 +0100

    Change x axis in cavity scan
    Possible to display now the frequency shift, or the round trip phase

--- Classes/@Cavity1/Display_Scan.m
+++ Classes/@Cavity1/Display_Scan.m
@@ -1,10 +1,20 @@
-function Display_Scan(Cin)
+function Display_Scan(Cin,varargin)
 % Display_scan() Display the scan of the cavity. Can show the different
 % mode excited in the cavity
 
-if ~isa(Cin, 'Cavity1')
-    error('Display_scan(C): The first and only argument must be an instance of the class Cavity1  ')
-end
+p  = inputParser;
+
+% Check if the first argument is an E_Field
+p.addRequired('Cin', @(x)isa(x, 'Cavity1'));
+
+% Horizontal axis of the scan
+p.addParameter('scan','length', @(x)strcmpi(x,'length') | ...
+    strcmpi(x,'frequency') | strcmpi(x,'RT phase') );
+
+% Check if the circulating is displayed with the SB or not
+p.addParameter('Display_with_SB',true,@(x)islogical(x));
+
+p.parse(Cin,varargin{:})
 
 %  Initialize and hide the GUI as it is being constructed.
 f = figure('Visible','off','Position',[100,300,800,700]);
@@ -14,15 +24,33 @@ hai1 = axes('Units','pixels','Position',[70,50,250,250]);
 hai2 = axes('Units','pixels','Position',[470,50,250,250]);
 
 % Check if we display the SB or not
-Display_with_SB = true;
+Display_with_SB = p.Results.Display_with_SB;
+
+% Build the horizontal axis
+if strcmp(p.Results.scan,'length') % display the horizontal axis as cavity length change
+    Coef_scale = 2;
+    Hor_axis = Cin.Cavity_scan_R(:,1) / Coef_scale;
+    Hor_title = 'Cavity length detuning [m]';
+elseif strcmp(p.Results.scan,'frequency') % display the horizontal axis as laser frequency change
+    Coef_scale = Cin.Laser_in.Wavelength / (299792458/(2*Cin.Length));
+    %Hor_axis = (Cin.Cavity_scan_R(:,1)/(Cin.Laser_in.Wavelength))*(299792458/(2*Cin.Length));
+    Hor_axis = Cin.Cavity_scan_R(:,1) / Coef_scale;
+    Hor_title = 'Cavity frequency shift [Hz]';
+else  % display the horizontal axis as cavity round trip phase change
+    %Hor_axis = (Cin.Cavity_scan_R(:,1)/(Cin.Laser_in.Wavelength))*2;
+    Coef_scale = Cin.Laser_in.Wavelength /2;
+    Hor_axis = Cin.Cavity_scan_R(:,1) / Coef_scale;
+    Hor_title = 'Cavity round trip phase shift [pi]';
+end
 
 % Top plot: the cavity scan
-semilogy(Cin.Cavity_scan_R(:,1),Cin.Cavity_scan_R(:,2),'Parent',hap1,'LineWidth',2)
+semilogy(Hor_axis,Cin.Cavity_scan_R(:,2),'Parent',hap1,'LineWidth',2)
 axis(hap1,'tight')
 title(hap1,'Circulating power vs resonance length')
 dcm_obj = datacursormode(f);
 set(dcm_obj,'UpdateFcn', @myupdatefcn )
-xlabel(hap1,'Cavity detuning [m]')
+xlabel(hap1,Hor_title)
+
 
 % Bottom left: the input beam
 imagesc(Cin.Laser_in.Grid.Axis,Cin.Laser_in.Grid.Axis,abs(Cin.Laser_in.Field).^2,'Parent',hai1)
@@ -74,10 +102,10 @@ set(f,'Visible','on')
             end
             
             for ii=1:num_iter
-                Field_reconstructed_car = Field_reconstructed_car + Cin.Cavity_scan_all_field(:,:,ii) * exp(1i*Cin.Laser_in.k_prop* length_reso*ii);
+                Field_reconstructed_car = Field_reconstructed_car + Cin.Cavity_scan_all_field(:,:,ii) * exp(-1i*Cin.Laser_in.k_prop* length_reso*ii*Coef_scale);
                 if Cin.Laser_in.Nb_Pair_SB
-                Field_reconstructed_SBu = Field_reconstructed_SBu + Cin.Cavity_scan_all_field(:,:,ii) * exp(1i*Cin.Laser_in.k_prop* length_reso*ii) * exp(1i*D_phi*ii);
-                Field_reconstructed_SBl = Field_reconstructed_SBl + Cin.Cavity_scan_all_field(:,:,ii) * exp(1i*Cin.Laser_in.k_prop* length_reso*ii) * exp(-1i*D_phi*ii);
+                    Field_reconstructed_SBu = Field_reconstructed_SBu + Cin.Cavity_scan_all_field(:,:,ii) * exp(-1i*Cin.Laser_in.k_prop* length_reso*ii*Coef_scale) * exp(1i*D_phi*ii);
+                    Field_reconstructed_SBl = Field_reconstructed_SBl + Cin.Cavity_scan_all_field(:,:,ii) * exp(-1i*Cin.Laser_in.k_prop* length_reso*ii*Coef_scale) * exp(-1i*D_phi*ii);
                 end
             end
             power_norm_SB =  Calculate_Power(Cin.Laser_in,'SB')/2;
@@ -87,7 +115,7 @@ set(f,'Visible','on')
         else
             Field_reconstructed = complex(zeros(Grid_num_point,Grid_num_point,'double'));
             for ii=1:num_iter
-                Field_reconstructed = Field_reconstructed + Cin.Cavity_scan_all_field(:,:,ii) * exp(1i*Cin.Laser_in.k_prop* length_reso*ii);
+                Field_reconstructed = Field_reconstructed + Cin.Cavity_scan_all_field(:,:,ii) * exp(-1i*Cin.Laser_in.k_prop* length_reso*ii*Coef_scale);
             end
             Field_reconstructed = abs(Field_reconstructed).^2;
         end
