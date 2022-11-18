20a70ad8c0783e11a8d4cdcc754b7fb36f96ff1e
Jerome Degallaix <j.degallaix@lma.in2p3.fr>
2022-11-18 17:03:20 +0100

    Update Remove_00_Part.m
    - remove the SB in the input field
    - adapt the wavelength in the field to subtract.

--- Classes/@E_Field/Remove_00_Part.m
+++ Classes/@E_Field/Remove_00_Part.m
@@ -4,6 +4,7 @@ function Eout = Remove_00_Part(Ein,varargin)
 %   substracted. Very useful to highligh the distortion of a beam
 %   Eout is normalised by setting the power of the input beam to 1
 %  The substraction of 2 beams can also be achieved Remove_00_part(E1,E2)
+%  !! ONLY WORK FOR THE CARRIER
 
 p  = inputParser;
 p.FunctionName = 'Display an E_Field object';
@@ -19,15 +20,17 @@ p.parse(Ein,varargin{:})
 Ein = p.Results.Ein;
 Esub = p.Results.E_sub;
 
-Ein = Normalise_E(Ein,1);
+Ein.Nb_Pair_SB = 0;
+
+Ein = Normalise_E(Ein,'Power',1);
 
 % First, fit the beam with a TEM00
 
 if isempty(Esub)
     [E00_w, E00_r] = Fit_TEM00(Ein);
-    E00 = E_Field(Ein.Grid,'w',E00_w,'R',E00_r);
+    E00 = E_Field(Ein.Grid,'w',E00_w,'R',E00_r,'Wavelength',Ein.Wavelength);
 else
-    E00 = Normalise_E(Esub,1);
+    E00 = Normalise_E(Esub,'Power',1);
 end
 
 diff_phase = angle(Calculate_Overlap(Ein,E00));
@@ -35,7 +38,7 @@ diff_phase = angle(Calculate_Overlap(Ein,E00));
 % To minimize the output, be sure the 2 beams interfer destructively
 Eout = Ein - E00 * exp(1i*diff_phase);
 
-E_plot(Eout)
+E_Plot(Eout)
 
 end
 
