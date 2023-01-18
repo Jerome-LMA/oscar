5183ef9609652ba1fc3e4966ef31fb76302c2e12
Jerome Degallaix <j.degallaix@lma.in2p3.fr>
2023-01-18 12:28:03 +0100

    Possibility to normalise SB too

--- Classes/@E_Field/Normalise_E.m
+++ Classes/@E_Field/Normalise_E.m
@@ -1,48 +1,52 @@
-function Eout = Normalise_E(Ein,varargin)
+function Eout = Normalise_E(Ein, varargin)
 % Normalise a E_field
 % Use Normalise_E(E_field)    Normalises the E_field to 1W
-%        Normalise_E(E_field,'power',P)    Set the E_field to have a
+%        Normalise_E(E_field,P)    Set the E_field to have a
 %        power of P Watt
-
 p  = inputParser;
 
 % Check if the first argument is an E_Field
 p.addRequired('Ein', @(x)isa(x, 'E_Field'));
 
-% Check which power we would like to normalise to, by default 1W
-p.addParameter('Power',1, @(x)(isreal(x) && ge(x,0)) );
+% Check if we normalise just the carrier or all
+p.addOptional('power', 1, @(x) isnumeric(x)  && (x>=0));
 
-
+% Check if we normalise just the carrier or all
+p.addOptional('include','carrier', @(x)strcmpi(x,'carrier') | strcmpi(x,'all') );
 
 p.parse(Ein,varargin{:})
 
-Eout = Ein;
-Amplitude_scaling = sqrt( p.Results.Power / Calculate_Power(Ein) );
-
-if exist('isgpuarray','file') % to be compatible with version < 2020b
-    Run_on_GPU = isgpuarray(Ein.Field);
-else
-    Run_on_GPU = false;
+if p.Results.power == 0
+    Eout.Field = complex(zeros(E.Grid.Num_point,Ein.Grid.Num_point));
+    if  Ein.Nb_Pair_SB
+        for ii=1:Ein.Nb_Pair_SB
+            Eout.SB(ii).Field_lower = Eout.Field;
+            Eout.SB(ii).Field_upper = Eout.Field;
+        end
+     end
+     return;
 end
 
-if Run_on_GPU
-    Eout.Field = arrayfun(@times,Eout.Field,Amplitude_scaling);
-    if Amplitude_scaling == 0 % put all the SBs to zero otherwise do not change them
+if strcmp(p.Results.include,'carrier')
+    Eout = Ein;
+    Eout.Field = Eout.Field / sqrt(Calculate_Power(Ein));
+    Eout.Field = Eout.Field * sqrt(p.Results.power);
+elseif strcmp(p.Results.include,'all')
+    Eout = Ein;
+    P_tot = Calculate_Power(Ein, 'include', 'all');
+    norm_pow = sqrt(p.Results.power / P_tot);
+    
+    Eout.Field = Eout.Field * norm_pow;
+    
+    if Ein.Nb_Pair_SB        
         for ii = 1:Ein.Nb_Pair_SB
-            Eout.SB(ii).Field_lower = zeros(Ein.Grid.Num_point,'gpuArray');
-            Eout.SB(ii).Field_upper = zeros(Ein.Grid.Num_point,'gpuArray');
+            Eout.SB(ii).Field_lower = Eout.SB(ii).Field_lower * norm_pow;
+            Eout.SB(ii).Field_upper = Eout.SB(ii).Field_upper * norm_pow;
         end
     end
-else
-    Eout.Field = Eout.Field * Amplitude_scaling;
-    if Amplitude_scaling == 0 % put all the SBs to zero otherwise do not change them
+end      
         
-        for ii = 1:Ein.Nb_Pair_SB
-            Eout.SB(ii).Field_lower = zeros(Ein.Grid.Num_point);
-            Eout.SB(ii).Field_upper = zeros(Ein.Grid.Num_point);
-        end
-    end
-end
+        
+        
 
-end
 
