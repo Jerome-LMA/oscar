b591b23e42d02382f8d0be68c82d8e03e06a6ef1
Jerome Degallaix <j.degallaix@lma.in2p3.fr>
2024-09-11 18:03:43 +0200

    Update PSD function

--- Classes/@Grid/Do_Virtual_Map.m
+++ Classes/@Grid/Do_Virtual_Map.m
@@ -1,36 +1,32 @@
-function [Map] = Do_Virtual_Map(G,Power_law,varargin)
-%    Do_Virtual_map() Create a synthetic map according to a parametrised PSD
+function [map] = Do_Virtual_Map(G,Power_law,varargin)
+%Do_Virtual_map() Create a synthetic map according to a parametrised PSD
 %   Grid is the Grid object for the calculations
-%   Power_law is a vector describing the PSD power law (see the function
-%   FitFunctionPSD for the description)
-%   Possibility to use a pre-defined PSD for example:
-%   map = Do_Virtual_Map(G1,'IBF')
-%   2024 version from M. Lejean and J. Degallaix
+%   Power_law is a vector of length 8 with the power law, see the function
+%   2024 version from M. Le Jean and J. Degallaix
 
 p = inputParser;
 p.FunctionName = 'Create a virtual map';
 
 % Check if the first argument is a grid
-addRequired(p,'G',@(x)isa(x, 'Grid'));
+addRequired(p, 'G', @(x)isa(x, 'Grid'));
 
 % Check if the PSD law is given
-addRequired(p,'Power_law',@(x)isnumeric(x) || ischar(x))
+addRequired(p,'Power_law', @(x)isnumeric(x) || ischar(x))
 
 % Check if the phase is given
-p.addParameter('phase',[],@(x)isnumeric(x));
+p.addParameter('phase',[], @(x)isnumeric(x));
 
-p.parse(G,Power_law,varargin{:})
+p.parse(G,Power_law, varargin{:})
 
 debug = false;
 
-
 if ~isnumeric(Power_law)
-    if strcmp(Power_law,'IBF')
-        Power_law_num = [8E5 20 385 5.8 0.9 4.4]; 
-    elseif strcmp(Power_law,'Standard')
-        Power_law_num = [1300 110 1.8 4.4];
-    elseif strcmp(Power_law,'Coated')
-        Power_law_num = [65.46,3.22e+02,8.90,2.84,0.51,3.70]; !! will be completed later with the right phase, do not use
+    if strcmp(Power_law, 'IBF')
+        Power_law_num = [1.100, 4.728, 43.542, 3.391e+02, 1.636, 3.333, 0.622, 4.016];
+    elseif strcmp(Power_law, 'Standard')
+        Power_law_num = [11.025, 28.826, 1.445e+02, 2.121, 1.066, 4.641];
+    elseif strcmp(Power_law, 'Coated')
+        Power_law_num = [99.833, 7.890, 24.510, 3.254e+02, 1.776, 7.165, 1.476, 5.807];
     else
         error('Do_Virtual_Map(): name of the PSD not accepted')
     end
@@ -41,27 +37,31 @@ end
 if ~isempty(p.Results.phase)
     Phase = p.Results.phase; % Given Phase
 else % Random Phase
-    M = zeros(G.Num_point);
+    Phase = zeros(G.Num_point);
     for i = 1 : G.Num_point
-        M(i,:) = pi - 2*pi*rand(1,G.Num_point); %generate random number for the row;
-        M(:,i) = -M(i,:); % take the same number and put it in the antisymmetric slot
+        Phase(i,:) = pi - 2*pi*rand(1, G.Num_point); % generate random number for the row;
+        Phase(:,i) = -Phase(i,:); % put the same numbers in the antisymmetric slots
     end
-    Phase = M;
 end
 
-FFT_Radius_Axis = G.Axis_FFT(G.Half_num_point+1:end); % will take the radius along the diagonal
+frequency_axis = G.Axis_FFT(G.Half_num_point+1:end); % will take the radius along the diagonal
+% Avoid frequency == 0
+
+if(frequency_axis(1,1)==0)
+    frequency_axis = frequency_axis(2:end);
+end
 
 % ---- Draw the PSD 1D on the new frequency vector ---- %
-
-PSD_1D  = FitFunctionPSD(FFT_Radius_Axis,Power_law_num);
-PSD_1D(1) = PSD_1D(2);
+PSD_1D  = FitFunctionPSD(frequency_axis, Power_law_num);
 
 % ---- Reconstruct PSD 2D assuming radial symmetry ---- %
-PSD_2D =  interp1(FFT_Radius_Axis,PSD_1D,G.D2_FFT_r,'linear',0);
+PSD_2D =  interp1(frequency_axis, PSD_1D, G.D2_FFT_r,'spline',0);
 
 % ---- Get the map of associated PSD 2D with Phase ---- %
-ASD = sqrt(PSD_2D.*G.Length^2/G.Step^2);
-Map = ifft2(ifftshift(ASD.*exp(1i*Phase)),'symmetric');
-Map = -1*Map; % To discuss 
+ASD = sqrt(PSD_2D / G.Step_sq) * G.Num_point;
+
+%ASD = sqrt(PSD_2D*G.Length^2)/G.Step^2;
+map = ifft2(ifftshift(ASD.*exp(1i*Phase)),'symmetric');
+map = -1*map; 
 
 end
--- Classes/@Interface/Plot_PSD.m
+++ Classes/@Interface/Plot_PSD.m
@@ -4,7 +4,7 @@ function [PSD_1D,xFreq] = Plot_PSD(I_in,varargin)
 % Interface
 % Adapted from a set of functions made by M. Galimberti in the years
 % 2010-2011 at LMA.
-% 2023 version from J. Degallaix and M. Lejean
+% 2024 version from J. Degallaix and M. Lejean
 
 p = inputParser;
 
@@ -15,7 +15,7 @@ p.addRequired('I_in', @(x)isa(x, 'Interface'));
 p.addParameter('diam',[],@(x)isnumeric(x) && x>0);
 
 % Check if the 1D PSD is calculated after the sum over one dimension (by default it is radial)
-p.addParameter('rect_1D',[],@(x)islogical(x));
+p.addParameter('radial',true,@(x)islogical(x));
 
 % Check if the 1D PSD should be plotted or not
 p.addParameter('display',true,@(x)islogical(x));
@@ -27,14 +27,11 @@ p.addParameter('window_Gaussian',[],@(x)isnumeric(x) && x>0);
 p.addParameter('RMS_display',false,@(x)islogical(x));
 
 p.parse(I_in,varargin{:})
+Calculation_radial = p.Results.radial;
 
-if isempty(p.Results.rect_1D)
-    Calculation_radial = true;
-else
-    Calculation_radial = ~p.Results.rect_1D;
-end
+
 % -------------------------------------------------------------------------------
-%            First prepare the map for the calculation
+%            Preprocessing the map for the calculation
 
 % Remove the NaN if any
 I_in.surface(isnan(I_in.surface)) = 0;
@@ -61,17 +58,23 @@ map = map .* mask;
 
 % Cut the map around the useful part (a square of length diam_PSD)
 ind_cut = find(abs(I_in.Grid.Axis) <= diam_PSD/2);
-map2 = map(ind_cut,ind_cut);
-map_grid_r2 = I_in.Grid.D2_square(ind_cut,ind_cut);
+map = map(ind_cut,ind_cut);
 N_point = length(ind_cut);
 
+% Redefine the grid for the calculation and calculate the frequency axis
+G0 = Grid(N_point,diam_PSD);
+freq_axis = G0.Axis_FFT((N_point/2+1):end); % take positive frequency
+df = max(diff(freq_axis));
+
+%figure(102);imagesc(map)
+
 if p.Results.RMS_display
-    fprintf('RMS^2 of the cropped surface map: \t %g \n',std(map2(:))^2) %RMS
+    fprintf('RMS^2 of the cropped surface map: \t %g \n',std(map(:))^2) %RMS !! we have added 0 in the corner
 end
 
 if isempty(p.Results.window_Gaussian)
     % Apply the window and normalize to keep the power constant
-    if exist('hann')
+    if exist('hann','builtin')
         w = hann(N_point);
     else
         w = 0.5 * (1 - cos(2*pi*(0:N_point-1)'/(N_point-1)));
@@ -82,104 +85,96 @@ if isempty(p.Results.window_Gaussian)
     m = w(:)*w(:).'; % Outer product window
 else
     rad_gauss = p.Results.window_Gaussian;
-    m = exp(- 2*map_grid_r2/rad_gauss^2);
+    m = exp(- 2*G0.D2_square/rad_gauss^2);
 end
-
-% radially symmetric Hann window taken from:
-% Quantitative characterization of surface
-% topography using spectral analysis
-% equation 15
-
-cut_axis = I_in.Grid.Axis(ind_cut);
-Hann_rad_sym = zeros(length(cut_axis));
-
-for ii = 1:length(cut_axis)
-    for jj = 1:length(cut_axis)
-        if (cut_axis(ii)^2 + cut_axis(jj)^2) < (cut_axis(end))^2
-            Hann_rad_sym(ii,jj) = 1 + cos( (2*pi*sqrt(cut_axis(ii)^2 + cut_axis(jj)^2)) /(2*cut_axis(end)) );
-        end
-    end
-end
-
-m =  Hann_rad_sym;
 
 P_window =  (sum(abs(m).^2, 'all')/N_point^2); % Power of the window
 
-map2 = map2.*m;
-map2 = map2 / sqrt(P_window);
+map = map.*m;
+map = map/sqrt(P_window);
+
 
 if p.Results.RMS_display
-    fprintf('RMS^2 of the cropped windowed surface map: \t %g \n',std(map2(:))^2) %RMS
+    fprintf('RMS^2 of the cropped windowed surface map: \t %g \n',std(map(:))^2) %RMS
 end
 
-%figure(2); imagesc(log(abs(fftshift(fft2(Hann_rad_sym))))); axis square
+%figure(103);imagesc(map)
+
 
 %------------------------------------------------------------------------
-% Calculate the PSD 2D then put it in 1D
+% Calculate the PSD 2D
 
-% Now can do the PSD 2D
-ASD_2D = fftshift(fft2((map2)))*(I_in.Grid.Step^2);  %
-PSD_2D = (abs(ASD_2D).^2)/(diam_PSD^2);
+FFT_2D_norm = fftshift(fft2(map))/N_point;
+FFT_2D_norm_sq = abs(FFT_2D_norm).^2;
+
+% According to the Parseval's theorem, the 2 following quantities must be
+% equal:
+%sum(FFT_2D_norm_sq(:))
+%sum(map.^2,'all')
 
 if p.Results.RMS_display
-    fprintf('RMS^2 of the 2D PSD: \t %g  \n',sum(PSD_2D(:))/(diam_PSD^2)) %RMS
+    fprintf('RMS^2 of the 2D FFT squared: \t %g  \n',sum(FFT_2D_norm_sq(:))/N_point^2) %RMS
 end
 
-xFreq = (0:N_point/2)/(0.5*diam_PSD);
-df = max(diff(xFreq));
+PSD_2D = FFT_2D_norm_sq*I_in.Grid.Step_sq; 
+% normalisation to match the equation RMS^2 = int int PSD_2D(fx, fy) * dfx * dfy
+% dfx, dfy = 1/L
+
+if p.Results.RMS_display
+    fprintf('RMS^2 derived from the 2D PSD: \t %g  \n',sum(PSD_2D(:))*df^2) %RMS
+end
+
+
+%------------------------------------------------------------------------
+% Converting the PSD 2D to PSD 1D
 
 if ~Calculation_radial % Pass in 1D by integrating along one direction
-    PSD_1D_2s = sum(PSD_2D,2)*(df*N_point);
-    PSD_1D = (PSD_1D_2s(1:N_point/2+1)+PSD_1D_2s(end:-1:N_point/2));
-    xFreq(1) = [];
-    PSD_1D = PSD_1D(2:end);
-    xFreq = xFreq(1:end)';
-    
-    if p.Results.RMS_display
-        fprintf('RMS^2 of the integrated rectangular 1D PSD: \t %g  \n',2*sum(PSD_1D)/(N_point)) %RMS
-    end
-    
     label_y_axis = 'Power Spectral Density [m^2/m^-1]';
-    
-    
-else % Pass in 1D by integrating along a constant frequency
-    %---- From PSD_2D to Spectral map ----%
-    Step = I_in.Grid.Step;
-    GridSize = size(PSD_2D,1);
-    Vector = 1:1:GridSize;
-    
-    %---- Radial Integration in frequency Domain ----%
-    FFT_Axis =  -1/(2*Step) + (Vector-1)/diam_PSD;
-    [FFT_X,FFT_Y] = meshgrid(FFT_Axis);
-    FFT_Metrix = sqrt(FFT_X.^2 + FFT_Y.^2);
-    FFT_Radius_Axis = FFT_Axis((GridSize/2+1):end);
-    dq = mean(diff(FFT_Radius_Axis));
-    PSD_1D  = zeros(1,(GridSize/2));
-    
-    for ii = 1:length(FFT_Radius_Axis)
-        ind_ring = find((FFT_Metrix>FFT_Radius_Axis(ii)) & (FFT_Metrix<=FFT_Radius_Axis(ii)+dq));
-        PSD_1D(ii) = sum(PSD_2D(ind_ring))/(length(ind_ring)*(Step^2));
-    end
+
+    PSD_1D_2s = sum(PSD_2D,2)*df;
+    PSD_1D = (PSD_1D_2s(1:N_point/2)+PSD_1D_2s(end:-1:N_point/2+1)); % sum with the positive and negative frequency
+     
+    PSD_1D = PSD_1D(2:end);
+    freq_axis = freq_axis(end:-1:2)';
     
     if p.Results.RMS_display
-        fprintf('RMS^2 of the integrated radial 1D PSD: \t %g  \n',sum(PSD_1D.*FFT_Radius_Axis)/sum(FFT_Radius_Axis(1:end))) %RMS
+        fprintf('RMS^2 of the integrated rectangular 1D PSD: \t %g  \n',sum(PSD_1D)*df) %RMS
     end
     
-    xFreq = FFT_Radius_Axis(2:end)';
-    PSD_1D = PSD_1D(2:end)';
-    
+
+else % Pass in 1D by averaging along rings at constant frequency
     label_y_axis = 'Power Spectral Density [m^3/m^-1]';
     
+    freq_coordinates = sqrt(G0.D2_FFT_square);
+    
+    PSD_1D = zeros(1,G0.Num_point/2);
+    for ii = 1:length(freq_axis)
+        ring = (freq_coordinates>freq_axis(ii)) & (freq_coordinates<=freq_axis(ii)+df); %
+        PSD_1D(ii) = mean(PSD_2D(ring));
+    end
+
+    
+    %  Dropping the first point because freq = 0 is not physical.
+    freq_axis = freq_axis(2:end)';
+    PSD_1D = PSD_1D(2:end)';
+
+    if p.Results.RMS_display
+        fprintf('RMS^2 of the integrated radial 1D PSD: \t %g  \n' ...
+            ,    2*pi*sum(PSD_1D.*freq_axis)*df); 
+        % if purely random map, correct for the factor 4/pi to take into
+        % account the missing corner when the taking the PSD 1D
+    end
 end
 
 if p.Results.display
     figure(1)
-    loglog(xFreq,PSD_1D,'linewidth',2); set(gca,'fontsize',14);grid on
+    loglog(freq_axis,PSD_1D,'linewidth',2); 
+    set(gca,'fontsize',14, 'XScale', 'log', 'YScale','log');
     xlabel('Spatial frequency [1/m]','FontSize',14)
     ylabel(label_y_axis,'FontSize',14)
+    grid on
     axis tight
 end
-
 
 end
 
